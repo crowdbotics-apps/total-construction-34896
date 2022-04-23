0148dce2f1256022cfa571f8c67dd715ce1bb570
Crowdbotics <team@crowdbotics.com>
2022-04-23 21:12:04 +0000

    Initial commit.

---
+++ backend/total_construction_34896/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for total_construction_34896 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'total_construction_34896.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "total_construction_34896",
+  url: "https://total_construction_34896.botics.co",
+  api: "https://total_construction_34896.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
